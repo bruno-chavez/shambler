099bfab1eeb4b394478eb556ef9731c4640b7fbe
bruno.chavez <darklaier@gmail.com>
2018-04-16 23:06:35 -0300

    Added comments for more clarity.

--- shambler.py
+++ shambler.py
@@ -2,25 +2,26 @@ original_file_name = input("Enter file name: ")
 json_file_name = input("Enter a name for the JSON file: ")
 json_key = input("Enter a key for the JSON file: ")
 
-
+# Places all lines of original file in a list.
 with open(original_file_name) as f:
     original_file_list = f.readlines()
-
 f.close()
 
 json_file = open(json_file_name + ".json", "w")
 
 
+# json_line creates the lines that are going to be placed on the json file.
 def json_line(original_line, key):
     new_line = '"' + key + '": "' + original_line + '"'
     return new_line
 
-
+# Eliminates the new-line character from all the list elements one at a time.
 counter1 = 0
 while counter1 < len(original_file_list):
     original_file_list[counter1] = original_file_list[counter1][0:len(original_file_list[counter1]) - 1]
     counter1 += 1
 
+# Replaces all the double quotes to single quotes.
 counter2 = 0
 while counter2 < len(original_file_list):
     new_string = ""
@@ -34,8 +35,10 @@ while counter2 < len(original_file_list):
 
 json_file.write("[\n")
 
+
 counter3 = 0
 while counter3 < len(original_file_list):
+    # The original file usually is populated with empty elements.
     if original_file_list[counter3] == "":
         counter3 += 1
         continue
