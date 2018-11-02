408249862f558f8913c16e76854f59ea1bcbe367
andresweber <andresmweber@gmail.com>
2018-11-02 18:30:28 -0400

    Added functionality for the user to enter arbitrary key/num pairs separated with commas. E.g: Enter a key to use in the JSON file: key1,4,key2,3,key3,5
    If they want to enter 3 keys that matches a text file with 12 valid lines.
    
    Added the function _resolve_key_list to segregate the logic for parsing user key input.
    
    It will also pad the number if the user entered too few key values or cut it short if there are too many keys.

--- shambler.py
+++ shambler.py
@@ -1,4 +1,5 @@
 import os
+from itertools import chain
 
 JSON_EXT = '.json'
 JSON_FOLDER = 'JSON_Files'
@@ -13,17 +14,18 @@ def shambler(source_file, target_filename, json_key):
 
     # Replaces all the double quotes to single quotes and strips trailing whitespace and removes empty lines
     source_file_lines = [line.rstrip().replace('\"', '\'')
-                         for line in source_file_lines if line != ""]
-
+                         for line in source_file_lines]
+    source_file_lines = [line for line in source_file_lines if line]
     json_file = open(target_filename, "w")
     json_file.write("[\n")
 
-    num_source_lines = len(source_file_lines)
-    
-    for i, line in enumerate(source_file_lines):
+    num_lines = len(source_file_lines)
+    keys = _resolve_key_list(json_key, num_lines)
+
+    for i, (line, key) in enumerate(zip(source_file_lines, keys)):
         json_file.write("\t{\n")
-        json_file.write('\t"{K}": "{V}"\n'.format(K=json_key, V=line))
-        json_file.write('\t}\n' if i == num_source_lines - 1 else '\t},\n')
+        json_file.write('\t"{K}": "{V}"\n'.format(K=key, V=line))
+        json_file.write('\t}\n' if i == num_lines - 1 else '\t},\n')
 
     json_file.write("]")
     json_file.close()
@@ -33,6 +35,8 @@ def shambler_interactive():
     # Necessary inputs for shambler to work with.
     file_path = input("Enter your input plain text file: ")
     json_file_name = input("Enter a name for the output JSON file: ")
+    # User has the option of inputting json_key in the format "key, number, key, number"
+    # So that they can specify keys and the number of uses of each key in order.
     json_key = input("Enter a key to use in the JSON file: ")
 
     # Checking whether the user has input file names, file paths
@@ -45,6 +49,26 @@ def shambler_interactive():
     shambler(file_path, json_file_name, json_key)
 
     print(json_file_name + " created successfully.")
+
+
+def _resolve_key_list(json_key_user_input, num_lines):
+    if ',' in json_key_user_input:
+        # Split user input by commas
+        keys = ','.join(json_key_user_input.split(', ')).split(',')
+        keys = zip(keys[::2], keys[1::2])
+
+        # Creating a list of each key entry multiplied by the number following it.
+        keys = list(chain.from_iterable(
+            [[key_pair[0]] * int(key_pair[1]) for key_pair in keys]))
+
+        # Now filling out the list with the last entry if it does not match the number of lines in the source file.
+        if len(keys) < num_lines:
+            print('You entered %d too few keys, padding with %s' %
+                  (abs(len(keys) - num_lines), keys[-1]))
+            keys = keys + [keys[-1]] * abs(len(keys) - num_lines)
+    else:
+        keys = (json_key_user_input, num_lines)
+    return keys
 
 
 def _resolve_relative_file(file_path, extension=''):
